f387d1df475afa8d495f5b03103820aa42d37a35
Stephen Owen <Stephen@foxdeploy.com>
2016-09-21 14:53:59 -0400

    vnext
    experimental code to convert DSC Resource params into clickable fields

---
+++ Convert-DSCSyntaxToFields.ps1
@@ -0,0 +1,27 @@
+﻿$sampleSyntax = ((Get-DscResource User -Syntax).Split("`n") -join "`n")
+
+#reference
+{"
+    UserName = [string]
+    [DependsOn = [string[]]]
+    [Description = [string]]
+    [Disabled = [bool]]
+    [Ensure = [string]{ Absent | Present }]
+    [FullName = [string]]
+    [Password = [PSCredential]]
+    [PasswordChangeNotAllowed = [bool]]
+    [PasswordChangeRequired = [bool]]
+    [PasswordNeverExpires = [bool]]
+    [PsDscRunAsCredential = [PSCredential]]
+" | ConvertFrom-StringData}
+
+$fields = $sampleSyntax.Split("`n")[2..(($sampleSyntax.Split("`n").length)-3)] | ConvertFrom-StringData
+
+foreach ($field in $fields){
+    $name = $field.Keys[0] 
+    if ($name -like "``[*"){$name = $name -replace '\['}
+    if ($field.Values[0] -like "*string*"){"$name should render as a textbox for strings"}
+    if ($field.Values[0] -like "*bool*")  {"$name should render as a radio true/false"}
+
+}
+
